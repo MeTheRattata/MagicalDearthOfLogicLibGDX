1321e7f8f1ffd6d7060bac3261a68d66d83258b3
MeTheRattata <metherat@gmail.com>
2016-06-10 11:09:48 -0400

    Added CombatScreen, equal to GameScreen at the moment. Began adding Stage compatibility doodads.

--- core/src/com/mygdx/magicaldearthoflogic/MDLGame.java
+++ core/src/com/mygdx/magicaldearthoflogic/MDLGame.java
@@ -1,11 +1,15 @@
 package com.mygdx.magicaldearthoflogic;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.assets.AssetManager;
 
 public class MDLGame extends Game
 {
+	public AssetManager manager;
+	
 	public void create() 
 	{
+		
 		setScreen(new GameScreen());
 	}
 
