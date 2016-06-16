fa85330dceae869bc14a6d5a5a059f10a51a3063
MeTheRattata <metherat@gmail.com>
2016-06-16 09:32:56 -0400

    Added takeDamage and modified draw method for Entity.

--- core/src/com/mygdx/magicaldearthoflogic/Entity.java
+++ core/src/com/mygdx/magicaldearthoflogic/Entity.java
@@ -7,33 +7,58 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 
 public class Entity extends Actor
 {
-	private String name;
 	int scale;
 	int width;
 	int height;
 	int health;
 	int maxHealth;
+	int textureRegionX;
+	int textureRegionY;
+	int damageFrames;
 	
 	private TextureRegion textureRegion;
 	private TextureRegion[][] textures;
 	
-	public Entity(String newName)
+	public Entity(String name)
 	{
 		super();
-		textureRegion = new TextureRegion((MDLGame.assets.get(newName + ".png", Texture.class)));
-		//region set to default
-		//textureRegion.setRegion(0, 0, 32, 32);
-		setPosition(0, 0);
+		textureRegion = new TextureRegion((MDLGame.assets.get(name + ".png", Texture.class)));
 		scale = 4;
 		width = 32;
 		height = 32;
-		
-		textures = textureRegion.split(32,32);
-		name = newName;
+		textures = textureRegion.split(width, height);
+		textureRegionX = 0;
+		textureRegionY = 0;
 	}
-	
+	/**
+	 * Decrements health based on the damage taken, then returns a boolean of whether the Entity has died or not
+	 * @param damage: damage dealt to the Entity, negative if damage, positive if healing
+	 * @return: true if dead, false if not dead
+	 */
+	public boolean takeDamage(int damage)
+	{
+		health += damage;
+		if(health > maxHealth)
+			health = maxHealth;
+		if(damage < 0) //If is damage taken and not healing
+		{
+			damageFrames += 30;
+			textureRegionX++;
+			
+		}
+		if(health <= 0)
+			return true;
+		else
+			return false;
+	}
 	public void draw(Batch batch, float parentAlpha)
 	{
-		batch.draw(textures[0][0], getX(), getY(), width*scale, height*scale);
+		batch.draw(textures[textureRegionX][textureRegionY], getX(), getY(), width*scale, height*scale);
+		if(damageFrames != 0)
+		{
+			damageFrames--;
+			if(damageFrames == 0)
+				textureRegionX--;
+		}
 	}
 }
